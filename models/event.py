047a6ee5857fa4721cb063ecd194b5b8ebe8b7d5
2dam <ikerclasetrabajos@gmail.com>
2022-12-16 14:10:10 +0100

    commit de evento

---
+++ models/event.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Event(models.Model):
+     _name = 'ofc_odoo.event'
+
+     name = fields.Char(Required = True)
+     activity = field.Char(digits = (5,2) ,help = "€" )
+     capacity = field.Integer(required = True)
+     date = field.Datetime(required = True)
+     place = field.Char(required = True)
+     price = field.Float()
+     admin = field.Many2one('res.User',required=True)
+     coments = field.One2many('ofc_odoo.coment','event',required=True)
+     clients = field.Many2many('res.User')
+     sponsors = field.Many2many('ofc_odoo.sponsor')
