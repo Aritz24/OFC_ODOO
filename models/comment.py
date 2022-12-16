0e7e8b395ea75bb30b1f2aa45a11b9ce5e0f9a64
JeanC22 <jeanpierrc22@gmail.com>
2022-12-16 14:17:17 +0100

    Comment models

--- models/comment.py
+++ models/comment.py
@@ -6,19 +6,12 @@ from odoo import models, fields, api
 class comment(models.Model):
     _name = 'ofc_odoo.comment'
 
-    name = fields.Char()
-    message = fields.Text()
+    name = fields.Char(required=True)
+    message = fields.Text(required=True)
     valoration = fields.Integer()
-    privacity = fields.Boolean()
+    privacity = fields.Boolean(required=True)
     data_modify = fields.Datatime()
-    data_publication = field.Datatime()
+    data_publication = field.Datatime(required=True)
     event = fields.many2one('ofc_odoo.event',string="Event",required=True)
     client = fields.many2one('res.User',string="Client",required=True)
     
-        
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
