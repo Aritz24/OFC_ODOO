49d33759bbdd07a045832e09db1aada6403a8986
JeanC22 <jeanpierrc22@gmail.com>
2023-01-27 14:20:09 +0100

    First commit

--- models/comment.py
+++ models/comment.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
+from odoo import api
+from odoo import exceptions
 from odoo import fields
 from odoo import models
-from odoo import exceptions
-from odoo import api
-from odoo.exceptions import ValidationError 
+from odoo.exceptions import ValidationError
+from datetime import datetime
 
 class Comment(models.Model):
     _name = 'ofc_odoo.comment'
@@ -13,30 +14,34 @@ class Comment(models.Model):
     message = fields.Text()
     valoration = fields.Integer()
     privacity = fields.Boolean()
-    date_publication = fields.Datetime()
+    date_publication = fields.Date()
     event = fields.Many2one('ofc_odoo.event', string="Events")
     comclient = fields.Many2one('res.users', required=True, ofc_admin=False)
     
     @api.onchange('date_publication')
     def _onchange_date_publication(self):
-        current_date = fields.Datetime.now()
-        print('current: %s' % current_date ,'date: %s' % self.date_publication)
-        if self.date_publication > current_date:
-            return{
-                'warning': {
-                    'title': "Bad date setted",
-                    'message': "The date isn't equals with the current date"
-                }
-            }   
-        if self.date_publication < current_date:
-            return{
-                'warning': {
-                    'title': "Bad date setted",
-                    'message': "The date isn't equals with the current date"
-                }
-            }   
+        current_date = fields.Date().today()
+        if self.date_publication:
+            if self.date_publication > current_date:
+                return{
+                    'warning': {
+                        'title': "Bad date setted",
+                        'message': "The date isn't equals with the current date"
+                    }
+                }   
+            if self.date_publication < current_date:
+                return{
+                    'warning': {
+                        'title': "Bad date setted",
+                        'message': "The date isn't equals with the current date"
+                    }
+                }   
+         
     @api.constrains('date_publication')
     def _check_date_publication(self):
-        current_date = fields.Datetime.now()
+        #current_date = datetime.datetime.now().date()
+        current_date = fields.Date().today()
         if self.date_publication > current_date:
-            raise ValidationError("BAD DATE %s" % self.date_publication)
+            raise ValidationError("BAD DATE %s %s" % (self.date_publication,current_date))
+        if self.date_publication < current_date:
+            raise ValidationError("BAD DATE %s %s" % (self.date_publication,current_date))
