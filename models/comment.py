9a4ec4b970e53790590bd5c5822709c7a04dcced
JeanC22 <jeanpierrc22@gmail.com>
2023-02-02 11:33:00 +0100

    finished without manifest

--- models/comment.py
+++ models/comment.py
@@ -1,21 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api
-from odoo import exceptions
 from odoo import fields
 from odoo import models
 from odoo.exceptions import ValidationError
-from datetime import datetime
 
 class Comment(models.Model):
     _name = 'ofc_odoo.comment'
 
-    name = fields.Char()
-    message = fields.Text()
+    name = fields.Char(required=True)
+    message = fields.Text(required=True)
     valoration = fields.Integer()
     privacity = fields.Boolean()
-    date_publication = fields.Date()
-    event = fields.Many2one('ofc_odoo.event', string="Events")
+    date_publication = fields.Date(required=True)
+    event = fields.Many2one('ofc_odoo.event', string="Events", required=True)
     comclient = fields.Many2one('res.users', required=True, ofc_admin=False)
     
     @api.onchange('date_publication')
@@ -39,9 +37,16 @@ class Comment(models.Model):
          
     @api.constrains('date_publication')
     def _check_date_publication(self):
-        #current_date = datetime.datetime.now().date()
         current_date = fields.Date().today()
         if self.date_publication > current_date:
-            raise ValidationError("BAD DATE %s %s" % (self.date_publication,current_date))
+            raise ValidationError("BAD DATE, Isn't current date")
         if self.date_publication < current_date:
-            raise ValidationError("BAD DATE %s %s" % (self.date_publication,current_date))
+            raise ValidationError("BAD DATE, Isn't current date")
+        
+    @api.constrains('valoration')
+    def _check_valoration(self):
+        print(self.valoration)
+        if self.valoration > 10:
+            raise ValidationError("Range is't 1 - 10 your rate: %s" % self.valoration)
+        if self.valoration < 0:
+            raise ValidationError("Range is't 1 - 10 your rate: %s" % self.valoration)    
